9150ed31d66c864f3369f9d725b9f4b9384669a5
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2019-12-14 18:44:15 +0700

    Add Even number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,25 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
+import javax.crypto.spec.PSource;
+
 public class Main {
     public static void lesson1() {
         for (int i = 1; i <= 7; i++) {
             System.out.println(i);
         }
+        System.out.println("--------------");
+    }
+    public static void evenNumber() {
+        for (int k = 2; k <= 15; k = k+2){
+            System.out.println(k);
+        }
+        System.out.println("--------------");
     }
 
     public static void main(String[] args) {
         lesson1();
+        evenNumber();
     }
 }
