e906c0da0e9be68a7dd3937c2f891530ae1aedc2
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2019-12-14 18:37:29 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void lesson1() {
+        for (int i = 1; i <= 7; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        lesson1();
+    }
+}
